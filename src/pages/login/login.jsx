0f9cd68bf9b076af1b15fc6b9267c295db337896
zaiaaa <gustavo.0801214@gmail.com>
2024-01-31 17:30:31 -0300

    fazendo a pagina de login com react-hook-form

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,68 @@
+import React, { useContext } from 'react'
+import "./login.css"
+import { Card } from '../../components/Card/card'
+import { Input } from '../../components/Input/input'
+import { Button } from '../../components/Button/button'
+
+import { useForm } from 'react-hook-form'
+import {yupResolver} from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import foto from "../../assets/logo.png"
+import { AuthContext } from '../../context/auth'
+
+const schema = yup.object({
+  login: yup.string().required('Campo obrigatório'),
+  senha: yup.string().required('Campo obrigatório')
+}).required()
+
+
+const Login = () => {
+
+  const {handleLogin} = useContext(AuthContext)
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({resolver: yupResolver(schema),
+    mode: 'onChange',
+})
+
+
+const onSubmit = async (formData) => {
+  handleLogin(formData)
+  console.log(formData)
+}
+  
+
+  return (
+    <div className='bg'>
+        <div className='ct-card'>
+          <Card variant={"purple"} width={"30rem"}>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className='ct-input'>
+                <div>
+                  <label htmlFor="login">Login</label>
+                  <Input name={"login"} control={control} placeholder={"Login"}/>
+                </div>
+                
+                <div>
+                  <label htmlFor="senha">Senha</label>
+                  <Input name={"senha"} control={control} type={"password"} placeholder={"Senha"}/>
+                </div>
+              </div>
+                <Button text={"Entrar"} variant={"green"} type={"submit"} width={"20%"}/>
+            </form>
+            <div className='ct-img'>
+              <img src={foto} alt="adwawd" />
+            </div>
+
+          </Card>
+        </div>
+
+    </div>
+  )
+}
+
+export {Login}
