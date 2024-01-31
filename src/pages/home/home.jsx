7aaadb30cf01a312081e9d6302672622001302bb
zaiaaa <gustavo.0801214@gmail.com>
2024-01-31 17:30:17 -0300

    fazendo a pagina home para testar a autenticação

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,22 @@
+import React, { useContext, useEffect } from 'react'
+import { AuthContext } from '../../context/auth'
+import { useNavigate } from 'react-router'
+
+export const Home = () => {
+  
+    const {isAuth, logoff} = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(!isAuth){
+            navigate('/')
+        }
+    }, [])
+    
+    return (
+        <>
+            <div>{isAuth ? "vc ta autenticado" : 'nao ta autenticado'}</div>
+            <button onClick={logoff}>deslogar</button>
+        </>
+    )
+}
