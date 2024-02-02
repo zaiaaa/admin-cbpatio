a3debef873155b7822a11f75cec0da2f9a5074e3
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-02 13:09:08 -0300

    retirando cosnole.log do isAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Login } from './pages/login/login'
-import { AuthContextProvider } from './context/auth'
-import {MainRoutes} from './components/MainRoutes/MainRoutes'
+import { AuthContext, AuthContextProvider } from './context/auth'
+import { MainRoutes } from './components/MainRoutes/MainRoutes'
 import { Layout } from './components/Layout/Layout'
+import { ChakraProvider } from '@chakra-ui/react'
+import { useContext } from 'react'
+
+
 
 function App() {
-
+  const { isAuth } = useContext(AuthContext)
+  console.log(isAuth)
   return (
     <Router>
       <AuthContextProvider>
-        <Routes>
-          <Route path='/' element={<Login />} />
-        </Routes>
+        <ChakraProvider>
+          <Routes>
+            <Route path='/' element={<Login />} />
+          </Routes>
           <Layout>
             <MainRoutes />
           </Layout>
+        </ChakraProvider>
       </AuthContextProvider>
     </Router>
   )
