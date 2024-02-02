3454f8eb63f2fd2f7096a64ab57f1dfebb6b0d5c
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-02 14:09:58 -0300

    apagando a existencia da inutil e infeliz rota home

--- src/components/MainRoutes/MainRoutes.jsx
+++ src/components/MainRoutes/MainRoutes.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom'
-import { Home } from '../../pages/home/home'
 import { PrivateRoute } from '../../services/privateRoute'
 import { Campeonatos } from '../../pages/campeonatos/Campeonatos'
 import { Valores } from '../../pages/valores/Valores'
@@ -10,10 +9,6 @@ const MainRoutes = () => {
     return (
         <>
             <Routes>
-                <Route path='/home' element={
-                    <PrivateRoute>
-                        <Home />
-                    </PrivateRoute>} />
                 <Route path='/campeonatos' element={
                     <PrivateRoute>
                         <Campeonatos />
