e2e1cb24794db47366414d327c0ea711297a6639
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-02 13:10:08 -0300

    encapsulando todo o componente em uma classe layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,25 +1,32 @@
 import './Layout.css'
 import { Link } from 'react-router-dom/dist'
+import { useEffect, useContext } from 'react'
+import foto from "../../assets/logo.png"
+import { AuthContext } from '../../context/auth'
 
 const Layout = ({ children }) => {
+
+
     return (
         <>
-            <header>
+            <div className="layout">
                 <div className="sidebar">
-                    <div className="logo"></div>
+                    <div className="logo">
+                        <img src={foto} alt="logo" />
+                    </div>
                     <nav>
                         <ul>
                             <Link to={"/campeonatos"}>Campeonatos</Link>
+                            <Link to={"/home"}>home</Link>
                         </ul>
                     </nav>
                 </div>
-            </header>
-                {children}
-            <footer>
-                footer content
-            </footer>
+                <div className="children">
+                    {children}
+                </div>
+            </div>
         </>
     )
 }
 
-export {Layout}
+export { Layout }
