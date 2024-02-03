c9d0a86ae757c7f51001e93c0c00d814e7adf9e0
zaiaaa <gustavo.0801214@gmail.com>
2024-02-02 21:03:31 -0300

    adicionando a variant purple pro botao ficar roxo e o padding pra setar o padding

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./button.css"
 
-const Button = ({type, variant, text, width}) => {
+const Button = ({type, variant, text, width, padding, onClick}) => {
   return (
     <div className='ct-btn'>
-      <button className={`button ${variant}`} type={type} style={{width}}>
+      <button className={`button ${variant}`} type={type} style={{width, padding}} onClick={onClick}>
           {text}
       </button>
     </div>
