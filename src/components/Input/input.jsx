86b87914496bce8cbd88ab90fd201ff1c01d5955
zaiaaa <gustavo.0801214@gmail.com>
2024-02-03 14:48:28 -0300

    adicionando suporte a inputs do tipo file e de data

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -2,14 +2,34 @@ import React from 'react'
 import { Controller } from 'react-hook-form'
 import "./input.css"
 
-const Input = ({name, control, disabled, readOnly, ...rest}) => {
+const Input = ({name, control, disabled, readOnly, type = 'text',...rest}) => {
   return (
     <>        
         <Controller name={name}
         control={control}
-        defaultValue={""}
+        defaultValue={type === 'file' ? null : ''}
         rules={{required: true}}
-        render={({field: {value, onChange}}) => <input className='input' value={value} onChange={onChange} disabled={disabled} readOnly={readOnly} {...rest}/>}
+        render={({field: {value, onChange}}) => type == 'file' ? 
+        //se for um input de file
+        <input
+        type={type}
+        onChange={(e) => onChange(e.target.files[0])}
+        disabled={disabled}
+        readOnly={readOnly}
+        {...rest}
+      />
+      //se for um input de data
+      : type == 'datetime-local' ? <input
+      type={type}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      disabled={disabled}
+      readOnly={readOnly}
+      {...rest}
+      /> 
+      
+      //se não for um input de file nem de data
+      : <input className='input' value={value} onChange={onChange} disabled={disabled} readOnly={readOnly} {...rest}/>}
         />
     </>
   )
