7e734b66261219373dea0418ea78ab62d600d49e
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-03 10:49:11 -0300

    criando compoente de slider com seus cards e que serao reutilizados em outrar paginas (ainda não esta dinamico)

---
+++ src/components/SliderCampeonatos/SliderCampeonatos.jsx
@@ -0,0 +1,70 @@
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './SliderCampeonatos.css';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
+
+import { CardCampeonato } from '../CardCampeonato/CardCampeonato';
+import bgImage from '../../assets/bg-admin.png'
+
+const SliderCampeonatos = () => {
+
+    return (
+        <>
+            <Swiper
+                slidesPerView={4}
+                centeredSlides={false}
+                spaceBetween={10}
+                pagination={{
+                    type: 'fraction',
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <CardCampeonato
+                        titulo={'FORTNITE BOXFIGHT 2V2'}
+                        variant={'campeonato'}
+                        width={'100%'}
+                        height={'100%'} 
+                        bgImage={bgImage}
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <CardCampeonato
+                        titulo={'FORTNITE BOXFIGHT 2V2'}
+                        variant={'preview'}
+                        width={'100%'}
+                        height={'100%'}
+                        bgImage={bgImage}
+                         />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <CardCampeonato
+                        titulo={'FORTNITE BOXFIGHT 2V2'}
+                        variant={'jogos'}
+                        width={'100%'}
+                        height={'100%'} 
+                        bgImage={bgImage}
+                        />
+                </SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
+
+        </>
+    );
+}
+
+export { SliderCampeonatos }
