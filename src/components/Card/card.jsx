594aa7cfa94348686aedec07a443f42408b42371
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-03 10:46:17 -0300

    recebendo props de url do backgorund image no card

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./card.css"
 
-const Card = ({children, variant, width}) => {
+
+const Card = ({children, variant, width, height, bgImage}) => {
+  const bgImageStyle = {
+    backgroundImage: `url(${bgImage})`,
+    
+  }
+
   return (
-    <div className={`card-${variant}`} style={{width}}>
+    <div className={`card-${variant}`} style={{width, height, ...bgImageStyle}}>
         {children}
     </div>
   )
