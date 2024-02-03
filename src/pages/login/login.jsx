50626accfc2b61bb6a5ce99ca61a47743f5543a4
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-03 09:11:25 -0300

    identando código

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -6,7 +6,7 @@ import { Input } from '../../components/Input/input'
 import { Button } from '../../components/Button/button'
 
 import { useForm } from 'react-hook-form'
-import {yupResolver} from "@hookform/resolvers/yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import foto from "../../assets/logo.png"
 import { AuthContext } from '../../context/auth'
@@ -19,60 +19,61 @@ const schema = yup.object({
 
 const Login = () => {
 
-  const {handleLogin, erros} = useContext(AuthContext)
+  const { handleLogin, erros } = useContext(AuthContext)
 
   const {
     control,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm({resolver: yupResolver(schema),
+  } = useForm({
+    resolver: yupResolver(schema),
     mode: 'onChange',
-})
+  })
 
 
-const onSubmit = async (formData) => {
-  try{
-    handleLogin(formData)
-  }catch(e){
-    console.log(e)
+  const onSubmit = async (formData) => {
+    try {
+      handleLogin(formData)
+    } catch (e) {
+      console.log(e)
+    }
+    console.log(formData)
   }
-  console.log(formData)
-}
 
-useEffect(() => {
-  console.log(erros)
-}, [erros])
-  
+  useEffect(() => {
+    console.log(erros)
+  }, [erros])
+
 
   return (
     <div className='bg'>
-        <div className='ct-card'>
-          <Card variant={"purple"} width={"30rem"}>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className='ct-input'>
-                <div>
-                  <label htmlFor="login">Login</label>
-                  <Input name={"login"} control={control} placeholder={"Login"} required/>
-                </div>
-                
-                <div>
-                  <label htmlFor="senha">Senha</label>
-                  <Input name={"senha"} control={control} type={"password"} placeholder={"Senha"}/>
-                </div>
+      <div className='ct-card'>
+        <Card variant={"purple"} width={"30rem"}>
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='ct-input'>
+              <div>
+                <label htmlFor="login">Login</label>
+                <Input name={"login"} control={control} placeholder={"Login"} />
               </div>
-                <Button text={"Entrar"} variant={"green"} type={"submit"} width={"20%"}/>
-            </form>
-            <div className='ct-img'>
-              <img src={foto} alt="adwawd" />
-            </div>
 
-          </Card>
-        </div>
-            {errors?.senha?.message}
+              <div>
+                <label htmlFor="senha">Senha</label>
+                <Input name={"senha"} control={control} type={"password"} placeholder={"Senha"} />
+              </div>
+            </div>
+            <Button text={"Entrar"} variant={"green"} type={"submit"} width={"20%"} />
+          </form>
+          <div className='ct-img'>
+            <img src={foto} alt="adwawd" />
+          </div>
+
+        </Card>
+      </div>
+      {errors?.senha?.message}
 
     </div>
   )
 }
 
-export {Login}
+export { Login }
