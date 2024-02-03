981d2ac62f5e477767bf59ff2aa20e3859101867
zaiaaa <gustavo.0801214@gmail.com>
2024-02-02 21:04:31 -0300

    testando o deslogar (ta funcionando)

--- src/pages/campeonatos/Campeonatos.jsx
+++ src/pages/campeonatos/Campeonatos.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react"
+import { Button } from "../../components/Button/button"
+import { AuthContext } from "../../context/auth"
 
 const Campeonatos = () => {
+    
+    const {logoff} = useContext(AuthContext)
+    
     return (
         <>
             <h1>Campeonatos</h1>
+            <Button text={"deslogar"} variant={"green"} type={"button"} onClick={logoff}/>
         </>
     )
 }
