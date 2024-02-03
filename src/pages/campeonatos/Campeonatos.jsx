08a5af3c63338f0d49c4d115a9d2b69fc3ed03e1
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-03 10:49:45 -0300

    importando sliderCampeonatos na pagina princiapal dos campeonatos

--- src/pages/campeonatos/Campeonatos.jsx
+++ src/pages/campeonatos/Campeonatos.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { Button } from "../../components/Button/button"
 import { AuthContext } from "../../context/auth"
 import { FormCadCampeonato } from "../../components/FormCadCampeonato/FormCadCampeonato"
+import { SliderCampeonatos } from "../../components/SliderCampeonatos/SliderCampeonatos"
 
 
 const Campeonatos = () => {
@@ -13,6 +14,7 @@ const Campeonatos = () => {
         <>
             <h1 >Campeonatos</h1>
             <h2 className="h2-sublinhado">CADASTRADOS</h2>
+            <SliderCampeonatos />
             <h2 className="h2-sublinhado">NOVO CAMPEONATO</h2>
             <FormCadCampeonato />
             <Button text={"deslogar"} variant={"green"} type={"button"} onClick={logoff} />
