d187fb877095cc67f2a5e21a2b13f36edb84ef3e
zaiaaa <gustavo.0801214@gmail.com>
2024-02-03 13:58:46 -0300

    adicionando a prop de margem

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./button.css"
 
-const Button = ({type, variant, text, width, padding, onClick}) => {
+const Button = ({type, variant, text, width, padding, margin, onClick}) => {
   return (
     <div className='ct-btn'>
-      <button className={`button ${variant}`} type={type} style={{width, padding}} onClick={onClick}>
+      <button className={`button ${variant}`} type={type} style={{width, padding, margin}} onClick={onClick}>
           {text}
       </button>
     </div>
