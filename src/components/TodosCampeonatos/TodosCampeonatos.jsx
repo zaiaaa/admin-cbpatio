6030ffce2a873e7d863b03a912d5b7c11dcac6ec
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-05 11:40:54 -0300

    criando componente da tela dos jogos, onde vai aparecer todos os campeonatos com os btns certos

---
+++ src/components/TodosCampeonatos/TodosCampeonatos.jsx
@@ -0,0 +1,80 @@
+import './TodosCampeonatos.css'
+import { CardCampeonato } from '../CardCampeonato/CardCampeonato'
+import img from '../../assets/noimage.png'
+
+const TodosCampeonatos = () => {
+    return (
+        <>
+            <div className="container">
+                <CardCampeonato
+                    titulo={'asaass'}
+                    variant={'jogos'}
+                    width={'214px'}
+                    height={'441px'}
+                    bgImage={img}
+
+                />
+                <CardCampeonato
+                    titulo={'asaass'}
+                    variant={'jogos'}
+                    width={'214px'}
+                    height={'441px'}
+                    bgImage={img}
+
+                />
+                <CardCampeonato
+                    titulo={'asaass'}
+                    variant={'jogos'}
+                    width={'214px'}
+                    height={'441px'}
+                    bgImage={img}
+
+                />
+                <CardCampeonato
+                    titulo={'asaass'}
+                    variant={'jogos'}
+                    width={'214px'}
+                    height={'441px'}
+                    bgImage={img}
+                />
+
+                <CardCampeonato
+                    titulo={'asaass'}
+                    variant={'jogos'}
+                    width={'214px'}
+                    height={'441px'}
+                    bgImage={img}
+
+                />
+                <CardCampeonato
+                    titulo={'asaass'}
+                    variant={'jogos'}
+                    width={'214px'}
+                    height={'441px'}
+                    bgImage={img}
+
+                />
+                <CardCampeonato
+                    titulo={'asaass'}
+                    variant={'jogos'}
+                    width={'214px'}
+                    height={'441px'}
+                    bgImage={img}
+
+                />
+                <CardCampeonato
+                    titulo={'asaass'}
+                    variant={'jogos'}
+                    width={'214px'}
+                    height={'441px'}
+                    bgImage={img}
+
+                />
+            </div>
+
+
+        </>
+    )
+}
+
+export { TodosCampeonatos }
