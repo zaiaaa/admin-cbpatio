99529dc135a7eebc692b2ec9d6fe9a3f5d20f1b8
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-05 08:27:35 -0300

    deixando o bg-image com blur

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import "./card.css"
 
 
-const Card = ({children, variant, width, height, bgImage}) => {
+const Card = ({ children, variant, width, height, bgImage }) => {
   const bgImageStyle = {
     backgroundImage: `url(${bgImage})`,
-    
+    filter: `blur(4px)`,
+    zIndex: '-1',
   }
 
   return (
-    <div className={`card-${variant}`} style={{width, height, ...bgImageStyle}}>
-        {children}
+    <div className={`card-${variant}`} style={{ width, height }}>
+      <div style={{ ...bgImageStyle, position: 'absolute', width: '100%', height: '100%', backgroundSize: 'cover' }}></div>
+      {children}
     </div>
   )
 }
 
-export {Card}
+export { Card }
