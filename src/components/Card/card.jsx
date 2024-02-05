2563f6ea00f3ed0854956f8e8596160a8b3cebaa
zaiaaa <gustavo.0801214@gmail.com>
2024-02-05 14:17:23 -0300

    deixando menos blur

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -5,7 +5,7 @@ import "./card.css"
 const Card = ({ children, variant, width, height, bgImage }) => {
   const bgImageStyle = {
     backgroundImage: `url(${bgImage})`,
-    filter: `blur(4px)`,
+    filter: `blur(2.5px)`,
     zIndex: '-1',
     position: 'absolute',
     width: '100%',
