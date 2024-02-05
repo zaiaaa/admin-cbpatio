cded525ede061c89e8533f6a398a147d3026d3f5
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-05 10:25:49 -0300

    adicionando spinner de loading para os campeonatos

--- src/components/SliderCampeonatos/SliderCampeonatos.jsx
+++ src/components/SliderCampeonatos/SliderCampeonatos.jsx
@@ -1,6 +1,7 @@
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Center, Spinner } from '@chakra-ui/react';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -25,12 +26,15 @@ import img from '../../assets/noimage.png'
 const SliderCampeonatos = ({ }) => {
 
     const [campeonatos, setCampeonatos] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const handleGetCampeonatos = async () => {
             try {
                 const fetch = await Api.get('/campeonatos')
                 setCampeonatos(fetch.data)
+                setLoading(false)
+
             } catch (e) {
 
             }
@@ -43,45 +47,59 @@ const SliderCampeonatos = ({ }) => {
 
     return (
         <>
-            <Swiper
-                slidesPerView={5}
-                centeredSlides={false}
-                spaceBetween={10}
-                pagination={{
-                    type: 'fraction',
-                }}
-                navigation={true}
-                modules={[Pagination, Navigation]}
-                className="mySwiper"
-            >
+            {
+                loading ? (
+                    <Center>
+                        <Spinner
+                            thickness='4px'
+                            speed='0.65s'
+                            color='#fff'
+                            size='xl'
+                        />
+                    </Center>
+                ) : (
+                    <Swiper
+                        slidesPerView={5}
+                        centeredSlides={false}
+                        spaceBetween={10}
+                        pagination={{
+                            type: 'fraction',
+                        }}
+                        navigation={true}
+                        modules={[Pagination, Navigation]}
+                        className="mySwiper"
+                    >
+                        {
 
-                {
-                    campeonatos.length !== 0 ? campeonatos.map((campeonato, index) => {
+                            campeonatos.length !== 0 ? campeonatos.map((campeonato, index) => {
 
-                        //OK //TODO UMA OPC QUE TENHA UMA FOTO DE QUE A PESSOA/CAMPEONATO NAO TEM FOTO, CASO NAO TENHA
-                        let foto = ''
-                        if (campeonato.foto) {
-                            foto = `${path}/${campeonato.foto.replace(/\\/g, '/')}`
-                        } else {
-                            foto = img
+                                //OK //TODO UMA OPC QUE TENHA UMA FOTO DE QUE A PESSOA/CAMPEONATO NAO TEM FOTO, CASO NAO TENHA
+                                let foto = ''
+                                if (campeonato.foto) {
+                                    foto = `${path}/${campeonato.foto.replace(/\\/g, '/')}`
+                                } else {
+                                    foto = img
+                                }
+
+                                return <SwiperSlide
+                                    key={index}
+                                >
+                                    <CardCampeonato
+                                        titulo={campeonato.nome}
+                                        variant={'campeonato'}
+                                        width={'100%'}
+                                        height={'100%'}
+                                        bgImage={foto}
+
+                                    />
+                                </SwiperSlide>
+                            }) : "Não existem campeonatos cadastrados"
                         }
 
-                        return <SwiperSlide
-                            key={index}
-                        >
-                            <CardCampeonato
-                                titulo={campeonato.nome}
-                                variant={'campeonato'}
-                                width={'100%'}
-                                height={'100%'}
-                                bgImage={foto}
+                    </Swiper>
+                )
+            }
 
-                            />
-                        </SwiperSlide>
-                    }) : "Não existem campeonatos cadastrados"
-                }
-
-            </Swiper>
 
         </>
     );
