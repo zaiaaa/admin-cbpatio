2421c686729008d1dedafdf3e9ff3896710cb158
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-05 11:41:42 -0300

    importnando componente onde sera renderizado todos os cards

--- src/pages/jogos/Jogos.jsx
+++ src/pages/jogos/Jogos.jsx
@@ -1,7 +1,12 @@
+import { TodosCampeonatos } from "../../components/TodosCampeonatos/TodosCampeonatos"
+
+TodosCampeonatos
+
 const Jogos = () =>{
     return(
         <>
             <h1>Jogos</h1>
+            <TodosCampeonatos/>
         </>
     )
 }
