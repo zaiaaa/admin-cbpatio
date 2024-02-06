83ac718f4e4f80849c5510395b77d45a948e8b75
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-06 11:00:33 -0300

    criando compoennte para edição de campeonato

--- src/pages/editarCampeonato/EditarCampeonato.jsx
+++ src/pages/editarCampeonato/EditarCampeonato.jsx
@@ -1,14 +1,92 @@
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
+import { Api } from '../../services/api';
+import { useEffect, useState } from "react";
+import { Spinner, Center } from '@chakra-ui/react';
+import { Button } from '../../components/Button/button';
+import { PreviewCampeonato } from "../../components/PreviewCampeonato/PreviewCampeonato";
+import { CardCampeonato } from '../../components/CardCampeonato/CardCampeonato'
+import './EditarCampeonato.css'
 
-const EditarCampeonato = () =>{
-    const {id} = useParams()
+const path = "http://localhost:3005"
+import img from '../../assets/noimage.png'
 
+const EditarCampeonato = () => {
+    const { id } = useParams()
+    const [campeonatos, setCampeonatos] = useState([])
+    const [loading, setLoading] = useState(true)
 
-    return(
+    useEffect(() => {
+        const handleGetCampeonatos = async () => {
+            try {
+                const fetch = await Api.get(`/campeonatos/id/${id}`)
+                setCampeonatos(fetch.data[0])
+                setLoading(false)
+
+            } catch (e) {
+
+            }
+        }
+        handleGetCampeonatos()
+    }, [])
+
+    let foto = ''
+    if (campeonatos.foto) {
+        foto = `${path}/${campeonatos.foto.replace(/\\/g, '/')}`
+    } else {
+        foto = img
+    }
+
+    return (
         <>
-            oie {id}
+            {
+                loading ? (
+                    <Center>
+                        <Spinner
+                            thickness='4px'
+                            speed='0.65s'
+                            color='#fff'
+                            size='xl'
+                        />
+                    </Center>
+                ) : (
+                    <>
+                        <h1>{campeonatos.nome}</h1>
+                        <div className="flex">
+                            <div className="first-row">
+                                <h2 className="h2-sublinhado">INFORMAÇÕES DO CAMPEONATO</h2>
+                                <PreviewCampeonato
+                                    nome={campeonatos.nome}
+                                    sinopse={campeonatos.sinopse}
+                                    data_hora={campeonatos.data}
+                                    modalidade={campeonatos.modalidade}
+                                    valor_ingresso={campeonatos.valor_entrada}
+                                    jogadores_time={campeonatos.jogadores}
+                                    premiacao={campeonatos.premiacao}
+                                    limite_inscricao={10}
+                                />
+                            </div>
+
+                            <div>
+                                <h3 >PREVIEW</h3>
+                                <CardCampeonato
+                                    idCamp={campeonatos.id_campeonato}
+                                    titulo={campeonatos.nome}
+                                    variant={'preview'}
+                                    width={'214px'}
+                                    height={'400px'}
+                                    bgImage={foto}
+                                />
+                            </div>
+                        </div>
+
+                    </>
+                )
+            }
+            <Link to='/campeonatos'>
+                <Button text={'Voltar'} type={"button"} variant={"red"} width={"10%"} padding={".75rem 2rem"} />
+            </Link>
         </>
     )
 }
 
-export {EditarCampeonato}
+export { EditarCampeonato }
