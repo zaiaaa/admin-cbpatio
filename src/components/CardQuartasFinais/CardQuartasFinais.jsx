96b6655f93d659a858ba7828f88bf2d41db63a94
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-10 15:40:13 -0300

    criando e estilzando o componente QuartasFinais

--- src/components/CardQuartasFinais/CardQuartasFinais.jsx
+++ src/components/CardQuartasFinais/CardQuartasFinais.jsx
@@ -1,9 +1,45 @@
-const CardQuartasFinais = () =>{
-    return(
+import './CardQuartasFinais.css'
+
+const CardQuartasFinais = () => {
+    return (
         <>
-        
+            <div className="quartas-fase">
+                <div className="fase-titulo">
+                    Quartas
+                </div>
+                <div className="fase-jogos">
+                    <div className="jogo">
+                        <div className="jogo-numero">jogo 1</div>
+                        <select name="jogo" id="jogo">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        VS.
+                        <select name="sla" id="sla">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                    <div className="jogo">
+                        <div className="jogo-numero">jogo 2</div>
+                        <select name="jogo" id="jogo">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        VS.
+                        <select name="sla" id="sla">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
 
-export{CardQuartasFinais}
+export { CardQuartasFinais }
