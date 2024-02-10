31919ef2cbfaef3ad2feb3c4ac4b34ce8f3ccb37
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-10 13:26:12 -0300

    criando o componente e deixando dinamico para ter o titulo na pagina

---
+++ src/pages/chaveCampeonato/ChaveCampeonato.jsx
@@ -0,0 +1,33 @@
+import { useParams } from "react-router-dom"
+import { Api } from "../../services/api"
+import { useEffect, useState } from "react"
+
+const ChaveCampeonato = () => {
+
+    const { id } = useParams()
+    const [campeonato, setCampeonato] = useState({})
+
+    useEffect(() => {
+
+        const getCamepeonatoData = async () => {
+            try {
+                const fetch = await Api.get(`campeonatos/id/${id}`)
+                console.log(fetch)
+                setCampeonato(fetch.data[0])
+                console.log(campeonato)
+            } catch(e) {
+                console.log(e)
+            }
+        }
+        getCamepeonatoData()
+
+    }, [])
+
+    return (
+        <>
+            <h1>{campeonato.nome}</h1>
+        </>
+    )
+}
+
+export { ChaveCampeonato }
