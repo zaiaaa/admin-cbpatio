2d4e4e44e613e1962b216963fabe659742a88267
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-10 15:39:37 -0300

    criando e estilizando o componente CardFinais

--- src/components/CardFinais/CardFinais.jsx
+++ src/components/CardFinais/CardFinais.jsx
@@ -1,9 +1,31 @@
-const CardFinais = () =>{
-    return(
+import './CardFinais.css'
+
+const CardFinais = () => {
+    return (
         <>
-        
+            <div className="finais-fase">
+                <div className="fase-titulo">
+                    Final
+                </div>
+                <div className="fase-jogos">
+                    <div className="jogo">
+                        <div className="jogo-numero">jogo 1</div>
+                        <select name="jogo" id="jogo">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        VS.
+                        <select name="sla" id="sla">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
 
-export{CardFinais}
+export { CardFinais }
