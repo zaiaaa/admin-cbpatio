4769d6f68057ab8124a6a22a488bd5cb85a65e34
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-10 15:40:28 -0300

    criando e estilizando o componente CArdSemiFinais

--- src/components/CardSemiFinais/CardSemiFinais.jsx
+++ src/components/CardSemiFinais/CardSemiFinais.jsx
@@ -1,9 +1,31 @@
-const CardSemiFinais = () =>{
-    return(
+import './CardSemiFinais.css'
+
+const CardSemiFinais = () => {
+    return (
         <>
-        
+            <div className="semis-fase">
+                <div className="fase-titulo">
+                    Semi
+                </div>
+                <div className="fase-jogos">
+                    <div className="jogo">
+                        <div className="jogo-numero">jogo 1</div>
+                        <select name="jogo" id="jogo">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        VS.
+                        <select name="sla" id="sla">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
 
-export{CardSemiFinais}
+export { CardSemiFinais }
