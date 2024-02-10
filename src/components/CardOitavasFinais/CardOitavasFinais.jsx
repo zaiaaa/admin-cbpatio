7dc4d2d4334e6cc9b9ceca01c2859fe4806e05f2
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-10 15:39:56 -0300

    criando e estilizando o componente CardOitavasFinais

--- src/components/CardOitavasFinais/CardOitavasFinais.jsx
+++ src/components/CardOitavasFinais/CardOitavasFinais.jsx
@@ -1,9 +1,74 @@
-const CardOitavasFinais = () =>{
-    return(
+import './CardOitavasFinais.css'
+
+const CardOitavasFinais = () => {
+    return (
         <>
-        
+            <div className="oitavas-fase">
+                <div className="fase-titulo">
+                    Oitavas
+                </div>
+                <div className="fase-jogos">
+                    <div className="jogo">
+                        <div className="jogo-numero">jogo 1</div>
+                        <select name="jogo" id="jogo">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        VS.
+                        <select name="sla" id="sla">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                    <div className="jogo">
+                        <div className="jogo-numero">jogo 2</div>
+                        <select name="jogo" id="jogo">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        VS.
+                        <select name="sla" id="sla">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                    <div className="jogo">
+                        <div className="jogo-numero">jogo 1</div>
+                        <select name="jogo" id="jogo">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        VS.
+                        <select name="sla" id="sla">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                    <div className="jogo">
+                        <div className="jogo-numero">jogo 2</div>
+                        <select name="jogo" id="jogo">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                        VS.
+                        <select name="sla" id="sla">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                    
+                </div>
+            </div>
         </>
     )
 }
 
-export{CarCardOitavasFinaisdFinais}
+export { CardOitavasFinais }
