9226d6b479e350a14a24669441c545f2684346fc
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-10 16:11:32 -0300

    deixando o btn de proxima fase e fase anterior funcional

--- src/components/Chave/Chave.jsx
+++ src/components/Chave/Chave.jsx
@@ -2,10 +2,27 @@ import './Chave.css'
 import { Button } from '../Button/button'
 import { CardOitavasFinais } from '../CardOitavasFinais/CardOitavasFinais'
 import { CardQuartasFinais } from '../CardQuartasFinais/CardQuartasFinais'
-import {CardSemiFinais} from '../CardSemiFinais/CardSemiFinais'
-import {CardFinais} from '../CardFinais/CardFinais'
+import { CardSemiFinais } from '../CardSemiFinais/CardSemiFinais'
+import { CardFinais } from '../CardFinais/CardFinais'
+import { useState } from 'react'
 
 const Chave = () => {
+    const [fase, setFase] = useState(1)
+
+    const minFase = 1;
+    const maxFase = 4;
+
+    const proxFase = () => {
+        if(fase < maxFase){
+            setFase(fase + 1)
+        }
+        
+    }
+    const faseAnterior = () => {
+        if(fase > minFase){
+             setFase(fase - 1)
+        }
+    }
     return (
         <>
             <div className="chave-page">
@@ -15,22 +32,22 @@ const Chave = () => {
                             chave 1
                         </div>
                         <div className="chave-fases">
-                            <CardOitavasFinais className={'active-div'}/>
-                            <CardQuartasFinais />
-                            <CardSemiFinais/>
+                            <CardOitavasFinais className={fase == 1 ? 'active-div' : ''} />
+                            <CardQuartasFinais className={fase == 2 ? 'active-div' : ''}/>
+                            <CardSemiFinais className={fase == 3 ? 'active-div' : ''}/>
                         </div>
                     </div>
                     <div className="final">
-                        <CardFinais/>
+                        <CardFinais className={fase == 4 ? 'active-div' : ''}/>
                     </div>
                     <div className="chave">
                         <div className="chave-nome">
                             chave 2
                         </div>
                         <div className="chave-fases">
-                            <CardSemiFinais/>
-                            <CardQuartasFinais />
-                            <CardOitavasFinais className='active-div'/>
+                            <CardSemiFinais className={fase == 3 ? 'active-div' : ''}/>
+                            <CardQuartasFinais className={fase == 2 ? 'active-div' : ''}/>
+                            <CardOitavasFinais className={fase == 1 ? 'active-div' : ''} />
                         </div>
                     </div>
                 </div>
@@ -41,8 +58,11 @@ const Chave = () => {
                         <Button text={'Ver eliminados'} type={'button'} variant={'red'} padding={'1rem'} width={'100%'}></Button>
                     </div>
                     <div className="mudar-fase">
-                        <Button text={'Proxima fase'} type={'button'} variant={'pink'} padding={'1rem'} width={'100%'}></Button>
-                        <Button text={'Fase Anterior'} type={'button'} variant={'pink'} padding={'1rem'} width={'100%'}></Button>
+                        <Button text={'Proxima fase'} type={'button'} variant={'pink'} padding={'1rem'} width={'100%'}
+                            onClick={proxFase}></Button>
+                        <Button text={'Fase Anterior'} type={'button'} variant={'pink'} padding={'1rem'} width={'100%'}
+                            onClick={faseAnterior}
+                        ></Button>
                     </div>
                 </div>
             </div >
