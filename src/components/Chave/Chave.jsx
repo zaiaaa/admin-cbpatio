dbbe1f288d63744cb3a1a7ff512c9137d2ee3a8f
zaiaaa <gustavo.0801214@gmail.com>
2024-02-11 04:12:08 -0300

    fazendo o put pra colocar os times nas oitavas

--- src/components/Chave/Chave.jsx
+++ src/components/Chave/Chave.jsx
@@ -5,9 +5,89 @@ import { CardQuartasFinais } from '../CardQuartasFinais/CardQuartasFinais'
 import { CardSemiFinais } from '../CardSemiFinais/CardSemiFinais'
 import { CardFinais } from '../CardFinais/CardFinais'
 import { useState } from 'react'
+import { Api } from '../../services/api'
+import { useParams } from 'react-router-dom'
 
 const Chave = () => {
     const [fase, setFase] = useState(1)
+    const [dadosJogoEsquerda, setDadosJogoEsquerda] = useState([])
+    const [dadosJogoDireita, setDadosJogoDireita] = useState([])
+
+    const {id} = useParams()
+
+    const getDadosJogoEsquerda = (data) => {
+        setDadosJogoEsquerda(data)
+    }
+
+    const getDadosJogoDireita = (data) => {
+        setDadosJogoDireita(data)
+    }
+
+    const handleSaveGames = async () => {
+        //console.log(dadosJogoDireita)
+        dadosJogoEsquerda.forEach(async (value, index) => {
+            const ids = value.id_time_and_time_campeonato
+            const splitId = ids.split(" ")
+            const id_time = splitId[0]
+            const id_time_campeonato = splitId[1]
+
+            const nomeJogo = value.name
+            const split = nomeJogo.split(" ")
+            const fase = split[2]
+            const jogo = split[1]
+            const chave = split[split.length - 1]
+
+            await Api.put(`/campeonatos/time/alterarTime/${id_time_campeonato}`, {
+                "fase": fase,
+                "jogo": jogo,
+                "chave": chave
+            })
+        })
+
+        dadosJogoDireita.forEach(async (value, index) => {
+            console.log(value.id_time)
+
+            const ids = value.id_time_and_time_campeonato
+            const splitId = ids.split(" ")
+            const id_time = splitId[0]
+            const id_time_campeonato = splitId[1]
+
+            const nomeJogo = value.name
+            const split = nomeJogo.split(" ")
+            const fase = split[2]
+            const jogo = split[1]
+            const chave = split[split.length - 1]
+
+            await Api.put(`/campeonatos/time/alterarTime/${id_time_campeonato}`, {
+                "fase": fase,
+                "jogo": jogo,
+                "chave": chave
+            })
+        })
+        
+        // for (let i = 0; i < dadosJogoDireita.length; i++) {
+        //     //console.log(id)
+        //     console.log(dadosJogoDireita[i].id_time)
+            
+        //     const nomeCamp = dadosJogoDireita[i].name
+        //     const split = nomeCamp.split(" ")
+        //     const fase = split[2]
+        //     const jogo = split[1]
+        //     const chave = split[split.length - 1]
+        //     //console.log(fase, jogo, chave)
+
+        //     await Api.post('/campeonatos/time/novoTime', {
+        //         "fk_id_time": dadosJogoDireita[i].id_time, 
+        //         "fk_id_campeonato": id,
+        //         "fase": fase,
+        //         "jogo": jogo,
+        //         "chave": chave
+        //     })
+        // }
+    }
+
+    // console.log(dadosJogoEsquerda)
+    // console.log(dadosJogoDireita)
 
     const minFase = 1;
     const maxFase = 4;
@@ -32,7 +112,7 @@ const Chave = () => {
                             chave 1
                         </div>
                         <div className="chave-fases">
-                            <CardOitavasFinais className={fase == 1 ? 'active-div' : ''} />
+                            <CardOitavasFinais className={fase == 1 ? 'active-div' : ''} getDadosJogo={getDadosJogoEsquerda} ladoChave={'esquerda'} />
                             <CardQuartasFinais className={fase == 2 ? 'active-div' : ''}/>
                             <CardSemiFinais className={fase == 3 ? 'active-div' : ''}/>
                         </div>
@@ -47,13 +127,13 @@ const Chave = () => {
                         <div className="chave-fases">
                             <CardSemiFinais className={fase == 3 ? 'active-div' : ''}/>
                             <CardQuartasFinais className={fase == 2 ? 'active-div' : ''}/>
-                            <CardOitavasFinais className={fase == 1 ? 'active-div' : ''} />
+                            <CardOitavasFinais className={fase == 1 ? 'active-div' : ''} getDadosJogo={getDadosJogoDireita} ladoChave={'direita'}/>
                         </div>
                     </div>
                 </div>
                 <div className="chaveamento-opcoes">
                     <div className="fase-opcoes">
-                        <Button text={'Salvar'} type={'button'} variant={'green'} padding={'1rem'} width={'100%'}></Button>
+                        <Button text={'Salvar'} type={'button'} variant={'green'} padding={'1rem'} width={'100%'} onClick={handleSaveGames} ></Button>
                         <Button text={'Limpar'} type={'button'} variant={'yellow'} padding={'1rem'} width={'100%'}></Button>
                         <Button text={'Ver eliminados'} type={'button'} variant={'red'} padding={'1rem'} width={'100%'}></Button>
                     </div>
