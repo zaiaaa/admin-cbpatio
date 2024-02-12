193e5d6459807b1c21549584adcbe9bfa98c6c18
zaiaaa <gustavo.0801214@gmail.com>
2024-02-12 03:31:11 -0300

    organizando os times que vão aparecer na tela separando-os por chave > esq, direita após isso, deixando tudo dinâmico e visual de acordo com o que o amigo cadastrar

--- src/components/CardOitavasFinais/CardOitavasFinais.jsx
+++ src/components/CardOitavasFinais/CardOitavasFinais.jsx
@@ -6,9 +6,13 @@ import { useEffect, useState } from 'react';
 const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
 
     const [selectedValues, setSelectedValues] = useState([]);
+    
     const [times, setTimes] = useState([])
-    const [oitavas, setOitavas] = useState([])
-    const [nomeOitavas, setNomeOitavas] = useState([])
+        
+    const [nomeChaves, setNomeChaves] = useState([])
+
+    const [chave, setChave] = useState({})
+    
     const [timesComNomes, setTimesComNomes] = useState([]);
     //const [IdtimesJaSelecionados, setIdTimesJaSelecionados] = useState([])
 
@@ -21,13 +25,21 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
             setTimes(data)
         }
 
-        const getOitavas = async () => {
-            const { data } = await Api.get('/campeonatos/time/times/fase/oitavas')
-            setOitavas(data)
+        const getChave = async () => {
+            const esq = await Api.get('/campeonatos/time/times/chave/esquerda')
+            const dir = await Api.get('/campeonatos/time/times/chave/direita')
+
+            setChave({
+                esquerda: esq.data,
+                direita: dir.data
+            })
         }
+
         getTeams()
-        getOitavas()
+        getChave()
     }, [])
+
+    console.log(chave)
 
     useEffect(() => {
         async function fetchNomesTimes() {
@@ -37,20 +49,39 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
                     return { id_time_campeonato: item.id_time_campeonato, id_time: item.fk_id_time, nome: data[0].nome };
                 })
             );
-            
-            const nomesOitavas = await Promise.all(
-                oitavas.map(async (item) => {
+
+            setTimesComNomes(nomesTimes);
+        }
+
+        fetchNomesTimes();
+    }, [times]);
+
+    useEffect(() => {
+        async function fetchNomesOitavas() {            
+            const nomesTimesChavesEsq = await Promise.all(
+                chave.esquerda.map(async (item) => {
                     const { data } = await Api.get(`/times/time/${item.fk_id_time}`)
                     return { id_time_campeonato: item.id_time_campeonato, id_time: item.fk_id_time, nome: data[0].nome };
                 })
             );
 
-            setTimesComNomes(nomesTimes);
-            setNomeOitavas(nomesOitavas)
+            const nomesTimesChavesDir = await Promise.all(
+                chave.direita.map(async (item) => {
+                    const { data } = await Api.get(`/times/time/${item.fk_id_time}`)
+                    return { id_time_campeonato: item.id_time_campeonato, id_time: item.fk_id_time, nome: data[0].nome };
+                })
+            );
+
+            setNomeChaves({
+                esquerda: nomesTimesChavesEsq,
+                direita: nomesTimesChavesDir
+            })
         }
 
-        fetchNomesTimes();
-    }, [times]);
+        fetchNomesOitavas()
+    }, [chave])
+
+    console.log(nomeChaves)
 
 
     const handleSelectChange = (id, name, event) => {
@@ -82,7 +113,7 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
         console.log(selectedValues)
     }, [selectedValues])
 
-    console.log(oitavas)
+    //console.log(chave.esquerda[0])
 
     return (
         <>
@@ -99,7 +130,16 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
 
                             {/* NA PARTE DO CLIENTE DEVEMOS FAZER A REQUEST SEM A CHAVE, JOGO E FASE.  */}
                             {/* VAMOS FAZER UM PUT E SETAR TODOS NAS OITAVAS, DEPOIS SÓ COM POSTS PRA PRÓXIMAS FASES */}
-                            {!oitavas || !oitavas[0] ? (<option value="">Selecione a equipe!</option>) : (<option value={oitavas ? oitavas[0]?.fk_id_time : "2"}>{nomeOitavas && ladoChave == 'esquerda' ? nomeOitavas[0]?.nome : nomeOitavas[8]?.nome}</option>) }
+                            
+
+                            {/* ITS FUCKING WORKING BITCHHHHHHHHH */}
+                            {
+                                (!chave || !chave.esquerda || !chave.esquerda[0]) ? (
+                                    <option value="">Selecione a equipe!</option>
+                                ) : (
+                                    <option value="">{ladoChave === 'esquerda' && nomeChaves?.esquerda ? nomeChaves?.esquerda[0]?.nome : ladoChave === 'direita' && nomeChaves?.direita ? nomeChaves?.direita[0]?.nome : ""}</option>
+                                )
+                            }
 
                             {timesComNomes.map((item, index) => (
                                 <option key={index} value={`${item.id_time} ${item.id_time_campeonato}`}>
@@ -109,8 +149,15 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
                         </select>
                         VS.
                         <select name="sla" id="sla" onChange={(e) => handleSelectChange(1, `jogo 1 oitavas (visitante) ${ladoChave}`, e)}>
-                        {!oitavas || !oitavas[0] ? (<option value="">Selecione a equipe!</option>) : (<option value={oitavas ? oitavas[1]?.fk_id_time : "2"}>{nomeOitavas ? nomeOitavas[1]?.nome : ""}</option>) }
- 
+                            
+                            {
+                                (!chave || !chave.esquerda || !chave.esquerda[1]) ? (
+                                    <option value="">Selecione a equipe!</option>
+                                ) : (
+                                    <option value="">{ladoChave === 'esquerda' && nomeChaves?.esquerda ? nomeChaves?.esquerda[1]?.nome : ladoChave === 'direita' && nomeChaves?.direita ? nomeChaves?.direita[1]?.nome : ""}</option>
+                                )
+                            } 
+
                             {timesComNomes.map((item, index) => (
                                 <option key={index} value={`${item.id_time} ${item.id_time_campeonato}`}>
                                     {item.nome}
@@ -121,8 +168,13 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
                     <div className="jogo">
                         <div className="jogo-numero">jogo 2</div>
                         <select name="jogo" id="jogo" onChange={(e) => handleSelectChange(2, `jogo 2 oitavas (casa) ${ladoChave}`, e)}>
-                        {!oitavas || !oitavas[0] ? (<option value="">Selecione a equipe!</option>) : (<option value={oitavas ? oitavas[2]?.fk_id_time : "2"}>{nomeOitavas ? nomeOitavas[2]?.nome : ""}</option>) }
-
+                            {
+                                (!chave || !chave.esquerda || !chave.esquerda[2]) ? (
+                                    <option value="">Selecione a equipe!</option>
+                                ) : (
+                                    <option value="">{ladoChave === 'esquerda' && nomeChaves?.esquerda ? nomeChaves?.esquerda[2]?.nome : ladoChave === 'direita' && nomeChaves?.direita ? nomeChaves?.direita[2]?.nome : ""}</option>
+                                )
+                            }
                             {timesComNomes.map((item, index) => (
                                 <option key={index} value={`${item.id_time} ${item.id_time_campeonato}`}>
                                     {item.nome}
@@ -131,8 +183,13 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
                         </select>
                         VS.
                         <select name="sla" id="sla" onChange={(e) => handleSelectChange(3, `jogo 2 oitavas (visitante) ${ladoChave}`, e)}>
-                        {!oitavas || !oitavas[0] ? (<option value="">Selecione a equipe!</option>) : (<option value={oitavas ? oitavas[3]?.fk_id_time : "2"}>{nomeOitavas ? nomeOitavas[3]?.nome : ""}</option>) }
-
+                            {
+                                (!chave || !chave.esquerda || !chave.esquerda[3]) ? (
+                                    <option value="">Selecione a equipe!</option>
+                                ) : (
+                                    <option value="">{ladoChave === 'esquerda' && nomeChaves?.esquerda ? nomeChaves?.esquerda[3]?.nome : ladoChave === 'direita' && nomeChaves?.direita ? nomeChaves?.direita[3]?.nome : ""}</option>
+                                )
+                            }
                             {timesComNomes.map((item, index) => (
                                 <option key={index} value={`${item.id_time} ${item.id_time_campeonato}`}>
                                     {item.nome}
@@ -143,7 +200,14 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
                     <div className="jogo">
                         <div className="jogo-numero">jogo 3</div>
                         <select name="jogo" id="jogo" onChange={(e) => handleSelectChange(4, `jogo 3 oitavas (casa) ${ladoChave}`, e)}>
-                        {!oitavas || !oitavas[0] ? (<option value="">Selecione a equipe!</option>) : (<option value={oitavas ? oitavas[4]?.fk_id_time : "2"}>{nomeOitavas ? nomeOitavas[4]?.nome : ""}</option>) }
+                            
+                            {
+                                (!chave || !chave.esquerda || !chave.esquerda[4]) ? (
+                                    <option value="">Selecione a equipe!</option>
+                                ) : (
+                                    <option value="">{ladoChave === 'esquerda' && nomeChaves?.esquerda ? nomeChaves?.esquerda[4]?.nome : ladoChave === 'direita' && nomeChaves?.direita ? nomeChaves?.direita[4]?.nome : ""}</option>
+                                )
+                            }
 
                             {timesComNomes.map((item, index) => (
                                 <option key={index} value={`${item.id_time} ${item.id_time_campeonato}`}>
@@ -153,7 +217,14 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
                         </select>
                         VS.
                         <select name="sla" id="sla" onChange={(e) => handleSelectChange(5, `jogo 3 oitavas (visitante) ${ladoChave}`, e)}>
-                        {!oitavas || !oitavas[0] ? (<option value="">Selecione a equipe!</option>) : (<option value={oitavas ? oitavas[5]?.fk_id_time : "2"}>{nomeOitavas ? nomeOitavas[5]?.nome : ""}</option>) }
+                            
+                            {
+                                (!chave || !chave.esquerda || !chave.esquerda[5]) ? (
+                                    <option value="">Selecione a equipe!</option>
+                                ) : (
+                                    <option value="">{ladoChave === 'esquerda' && nomeChaves?.esquerda ? nomeChaves?.esquerda[5]?.nome : ladoChave === 'direita' && nomeChaves?.direita ? nomeChaves?.direita[5]?.nome : ""}</option>
+                                )
+                            }
 
                             {timesComNomes.map((item, index) => (
                                 <option key={index} value={`${item.id_time} ${item.id_time_campeonato}`}>
@@ -165,7 +236,14 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
                     <div className="jogo">
                         <div className="jogo-numero">jogo 4</div>
                         <select name="jogo" id="jogo" onChange={(e) => handleSelectChange(6, `jogo 4 oitavas (casa) ${ladoChave}`, e)}>
-                        {!oitavas || !oitavas[0] ? (<option value="">Selecione a equipe!</option>) : (<option value={oitavas ? oitavas[6]?.fk_id_time : "2"}>{nomeOitavas ? nomeOitavas[6]?.nome : ""}</option>) }
+                            
+                            {
+                                (!chave || !chave.esquerda || !chave.esquerda[6]) ? (
+                                    <option value="">Selecione a equipe!</option>
+                                ) : (
+                                    <option value="">{ladoChave === 'esquerda' && nomeChaves?.esquerda ? nomeChaves?.esquerda[6]?.nome : ladoChave === 'direita' && nomeChaves?.direita ? nomeChaves?.direita[6]?.nome : ""}</option>
+                                )
+                            }
 
                             {timesComNomes.map((item, index) => (
                                 <option key={index} value={`${item.id_time} ${item.id_time_campeonato}`}>
@@ -175,8 +253,13 @@ const CardOitavasFinais = ({ className, getDadosJogo, ladoChave }) => {
                         </select>
                         VS.
                         <select name="sla" id="sla" onChange={(e) => handleSelectChange(7, `jogo 4 oitavas (visitante) ${ladoChave}`, e)}>
-                        {!oitavas || !oitavas[0] ? (<option value="">Selecione a equipe!</option>) : (<option value={oitavas ? oitavas[7]?.fk_id_time : "2"}>{nomeOitavas ? nomeOitavas[7]?.nome : ""}</option>) }
-
+                            {
+                                (!chave || !chave.esquerda || !chave.esquerda[0]) ? (
+                                    <option value="">Selecione a equipe!</option>
+                                ) : (
+                                    <option value="">{ladoChave === 'esquerda' && nomeChaves?.esquerda ? nomeChaves?.esquerda[7]?.nome : ladoChave === 'direita' && nomeChaves?.direita ? nomeChaves?.direita[7]?.nome : ""}</option>
+                                )
+                            }
                             {timesComNomes.map((item, index) => (
                                 <option key={index} value={`${item.id_time} ${item.id_time_campeonato}`}>
                                     {item.nome}
