3983470f95b41e8faecd104148e9dcf8d44842d6
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-20 21:39:28 -0300

    importando o card dos valores e adicionando uma ul com os itens

--- src/pages/valoresCampeonato/ValoresCampeonato.jsx
+++ src/pages/valoresCampeonato/ValoresCampeonato.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import { Api } from "../../services/api"
 import { useEffect, useState } from "react"
+import { CardValores } from "../../components/CardValores/CardValores"
 
 const ValoresCampeonato = () => {
 
@@ -14,7 +15,7 @@ const ValoresCampeonato = () => {
                 const fetch = await Api.get(`campeonatos/id/${id}`)
                 console.log(fetch)
                 setCampeonato(fetch.data[0])
-            } catch(e) {
+            } catch (e) {
                 console.log(e)
             }
         }
@@ -25,6 +26,14 @@ const ValoresCampeonato = () => {
     return (
         <>
             <h1>{campeonato.nome}</h1>
+            <CardValores titulo={'VALORES'}>
+                <ul>
+                    <li>VALOR ARRECADADO: <span className="valores">111.10  R$</span></li>
+                    <li>VALOR EM PREMIAÇÕES: <span className="valores">1111</span></li>
+                    <li>VALOR DA INSCRIÇÃO: <span className="valores">1111</span></li>
+                    <li>TIMES INSCRITOS: <span className="times-inscritos">1111</span></li>
+                </ul>
+            </CardValores>
         </>
     )
 }
