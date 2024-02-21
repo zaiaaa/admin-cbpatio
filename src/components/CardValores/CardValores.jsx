c448cc12492e92889a2b9d32660a176ca9d202c8
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-20 21:38:52 -0300

    criando o componente do card dos valores

---
+++ src/components/CardValores/CardValores.jsx
@@ -0,0 +1,19 @@
+import './CardValores.css'
+const CardValores = ({ titulo, children }) => {
+    return (
+        <>
+            <div className="card-valores">
+                <h1 className="card-valores-titulo">
+                    {titulo}
+                </h1>
+                {
+                    children
+                }
+            </div>
+
+        </>
+    )
+}
+
+
+export { CardValores }
