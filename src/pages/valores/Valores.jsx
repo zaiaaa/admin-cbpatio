575e23950b00a5fda1f364f84d787b976fbf2034
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-20 21:41:01 -0300

    importando o componente na pagina principal dos valores

--- src/pages/valores/Valores.jsx
+++ src/pages/valores/Valores.jsx
@@ -3,7 +3,13 @@ const Valores = () => {
     return (
         <>
             <h1>Valores</h1>
-            {/* <CardValores /> */}
+            <CardValores titulo={'GERAL'}>
+                <ul>
+                    <li>VALOR ARRECADADO: <span className="valores">111.10  R$</span></li>
+                    <li>VALOR EM PREMIAÇÕES: <span className="valores">1111</span></li>
+                    <li>MÉDIA DE VALOR ARRECADADO <span className="valores">1111</span></li>
+                </ul>
+            </CardValores>
         </>
     )
 }
