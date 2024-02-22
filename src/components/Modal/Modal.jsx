5473385bd73a4ceb4f8d8d9896ddf42a99adaaef
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-21 21:18:28 -0300

    criando o componente do modal para ser reutilizado

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,46 @@
+import {
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
+import { Button } from '../Button/button'
+
+const ModalComponent = ({ children, titulo, textModalOpenBtn, variantTextOpenBtn, handleOnClickSaveButton }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    return (
+        <>
+            <Button text={textModalOpenBtn} type={'button'} variant={variantTextOpenBtn} padding={'1rem'} width={'100%'} onClick={onOpen} />
+
+            <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose} size={'xl'}>
+                <ModalOverlay
+                    backdropFilter='auto'
+                    backdropBlur='1.5px' />
+                <ModalContent backgroundColor={'#1B1D21'}>
+                    <ModalHeader>{titulo}</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        {children}
+                    </ModalBody>
+
+                    <ModalFooter>
+                        {
+                            handleOnClickSaveButton && (
+                                <Button text={'Salvar'} type={'button'} variant={'green'} padding={'1rem'} width={'100%'}
+                                    onClick={handleOnClickSaveButton} />
+                            )
+                        }
+                        <Button text={'Cancelar'} type={'button'} variant={'red'} padding={'1rem'} margin={'10px'} width={'100%'} onClick={onClose} />
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+}
+
+export { ModalComponent }
