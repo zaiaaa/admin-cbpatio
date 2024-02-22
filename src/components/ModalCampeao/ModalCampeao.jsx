069d2497b5ff033ef58dee659f1fbd8fe279b4c5
zaiaaa <gustavo.0801214@gmail.com>
2024-02-22 20:32:53 -0300

    deixando o modal de campeão funcional

--- src/components/ModalCampeao/ModalCampeao.jsx
+++ src/components/ModalCampeao/ModalCampeao.jsx
@@ -1,11 +1,96 @@
+import { useEffect, useState } from "react"
 import { ModalComponent } from "../Modal/Modal"
 import './ModalCampeao.css'
+import { Api } from "../../services/api"
+import { useParams } from "react-router-dom"
 
 const ModalCampeao = () => {
-    const handleOnClickSaveButton = () => {
-        alert('KKKKKKKK quem ler gosta de dar o bozó')
+
+    const { id } = useParams()
+    
+    const [campeao, setCampeao] = useState({})
+    const [finalistas, setFinalistas] = useState({})
+    const [times, setTimes] = useState({})
+    const [selectedValue, setSelectedValue] = useState([])
+
+    const findEliminado = async (jogo, faseAnterior, faseAtual, chave, chaveTarget) => {
+        const timeEliminado = await Api.get(`/campeonatos/time/times/eliminados/${id}`, {
+            params: {
+                "jogo": jogo,
+                "faseAnterior": faseAnterior,
+                "faseAtual": faseAtual,
+                "chave": chave,
+                "chaveTarget": chaveTarget
+            },
+
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        //console.log(timeEliminado)
+
+        await Api.post(`/campeonatos/time/novoTime`, {
+            "fk_id_time": timeEliminado.data[0].fk_id_time,
+            "fk_id_campeonato": id,
+            "fase": `eliminado ${faseAnterior}`,
+            "jogo": ""
+        })
+    }
+    
+    
+    
+    const handleOnClickSaveButton = async () => {
+        const ids = selectedValue[0].id_time_and_campeonato
+        console.log(ids)
+        const split = ids.split(" ")
+        const time = split[0]
+        const camp = split[1]
+
+        try {
+            await Api.post("/campeonatos/time/novoTime", {
+                "fk_id_time": time,
+                "fk_id_campeonato": camp,
+                "fase": "campeao",
+                "jogo": ""
+            })
+
+            findEliminado(1, "final", "acabou", "esquerda", "esquerda")
+        } catch (error) {
+            alert("Erro -> ", error)
+        }
+
+        alert(`o time selecionado foi campeão!`)
     }
 
+    const handleSelectChange = (id, name, event) => {
+        const newValue = { "id": id, "id_time_and_campeonato": event.target.value, "name": name };
+        setSelectedValue(prevState => {
+            const filteredValues = prevState.filter(value => value.id !== id);
+            return [...filteredValues, newValue];
+        }
+        )
+    };
+
+    useEffect(() => {
+        console.log(selectedValue)
+    }, [selectedValue])
+
+    useEffect(() => {
+        const getTeams = async () => {
+            const {data: finalistas} = await Api.get(`/campeonatos/time/times/fase/final/${id}`)             
+            //a gnt vai usar essa opção pro fall guys, como é tudo sem fase, seleciona ele
+            const {data: times} = await Api.get(`/campeonatos/time/times/${id}`) 
+
+            setFinalistas(finalistas)
+            setTimes(times)
+        }
+
+        getTeams()
+    }, [])
+
+    console.log("TIMES: ", times)
+    console.log("FINAL: ", finalistas)
 
     return (
         <>
@@ -15,14 +100,16 @@ const ModalCampeao = () => {
                 variantTextOpenBtn={'orange'}
                 handleOnClickSaveButton={handleOnClickSaveButton}
             >
-                <select name="campeao" id="campeao" className="select-campeao">
-                    <option value="">
-                        Selecione o otário do campeão:
-                    </option>
-                    <option value={`sei la`}>
-                        times
-                    </option>
+                <select name="campeao" id="campeao" className="select-campeao" onChange={(e) => handleSelectChange(0, `campeao`, e)}>
+                    <option value="">Selecione o campeão</option>
+                    
+                    {finalistas && finalistas[0] ? finalistas.map((finalista) => (
+                        <option value={`${finalista.id_time} ${finalista.fk_id_campeonato}`}>
+                            {finalista.nome}
+                        </option>
+                    )) : null}
                 </select>
+
             </ModalComponent>
         </>
     )
