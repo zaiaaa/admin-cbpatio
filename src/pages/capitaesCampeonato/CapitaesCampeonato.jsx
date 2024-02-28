a4a944de71e83b3a2962440fd9ac201fb3cf2b1d
zaiaaa <gustavo.0801214@gmail.com>
2024-02-28 01:14:29 -0300

    deixando o ver capitaes dinâmico

--- src/pages/capitaesCampeonato/CapitaesCampeonato.jsx
+++ src/pages/capitaesCampeonato/CapitaesCampeonato.jsx
@@ -16,74 +16,19 @@ const CapitaesCampeonato = () => {
 
     const { id } = useParams()
     const [campeonato, setCampeonato] = useState({})
-    const [timesOitavas, setTimesOitavas] = useState([])
-    const [times, setTimes] = useState([])
-    const [timeCapitain, SetTimeCapitain] = useState([])
-    const {token} = useContext(AuthContext)
-
+    const [capitaes, setCapitaes] = useState([])
 
     useEffect(() => {
-
-        const getCamepeonatoData = async () => {
+        const getCapitaes = async () => {
             try {
-                const fetch = await Api.get(`campeonatos/id/${id}`)
-                setCampeonato(fetch.data[0])
+                const {data: capitaes} = await Api.get(`/campeonatos/time/times/capitaes/${id}`)
+                setCapitaes(capitaes)
             } catch (e) {
-                console.log(e)
+                console.error(e)
             }
         }
 
-       
-
-        const getTimesByCampeonato = async () => {
-            try {
-                // O FETCH AQUI TEM QUE PEGAR OS DADOS DOS TIMES INSCRITOS NO CAMPEONATO 
-                const fetch = await Api.get(`/campeonatos/time/times/fase/oitavas/${id}`)
-                setTimesOitavas(fetch.data)
-
-               timesOitavas.map(async (time) => {
-                    const fkIdTime = time.fk_id_time
-
-                    const fetchTime = await Api.get(`/times/time/${fkIdTime}`)
-                    
-                    setTimes(prevTimes => ([...prevTimes, fetchTime.data]));
-                })
-            } catch (e) {
-                console.log(e)
-            }
-        }
-
-       
-
-        const getCapitainsByTime = async () => {
-            try {
-                Api.defaults.headers.common['Authorization'] = `Bearer ${token.token}`;
-
-                times.map(async (time) => {
-                    const fkIdCaptain = time.fk_id_capitao;
-                    console.log(time)
-                    const fetchCapitain = await Api.get(`/usuarios/${fkIdCaptain}`);
-
-                    const newCapitain = {
-                        id: time.id_time,
-                        nome: fetchCapitain.nome,
-                        email: fetchCapitain.email,
-                        time: time.nome
-                    }
-                    SetTimeCapitain(prevCapitains => [...prevCapitains, newCapitain])
-                })
-
-            } catch (e) {
-                console.log(e);
-            }
-        }
-
-
-        getCamepeonatoData()
-        getTimesByCampeonato()
-        getCapitainsByTime()
-
-
+        getCapitaes()
     }, [])
 
 
@@ -91,6 +36,11 @@ const CapitaesCampeonato = () => {
         <>
             <h1>{campeonato.nome}</h1>
             <h2 className="h2-sublinhado">CAPITÃES DOS TIMES</h2>
+
+            {
+                capitaes.length > 0 
+                
+                ?
 
             <TableContainer>
                 <Table size='md'>
@@ -101,21 +51,29 @@ const CapitaesCampeonato = () => {
                             <Th color={'#7662F1'}>Time</Th>
                         </Tr>
                     </Thead>
+                    
                     <Tbody>
                         {
-                            timeCapitain.map((capitain) => (
-                                    <Tr key={capitain.id}>
-                                        <Td>{capitain.nome}</Td>
-                                        <Td>{capitain.email}</Td>
-                                        <Td>{capitain.time}</Td>
+                            capitaes.map((capitain) => (
+                                    <Tr key={capitain.id_time}>
+                                        <Td>{capitain.nome_usuario}</Td>
+                                        <Td>{capitain.celular ? capitain.celular : "Sem celular cadastrado"}</Td>
+                                        <Td>{capitain.nome_time}</Td>
                                     </Tr>
                             ))
 
                         }
-
                     </Tbody>
                 </Table>
             </TableContainer>
+
+            :
+
+            <h2>Sem times ainda..</h2>
+
+            }
+
+
         </>
     )
 }
