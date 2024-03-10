4b6142b8ae3482fc6a68ff9b30fe2370618383ac
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-10 10:47:17 -0300

    adicionando um loading na parte dos capitães

--- src/pages/capitaesCampeonato/CapitaesCampeonato.jsx
+++ src/pages/capitaesCampeonato/CapitaesCampeonato.jsx
@@ -9,26 +9,31 @@ import {
     Th,
     Td,
     TableContainer,
+    Center,
+    Spinner
 } from '@chakra-ui/react'
 import { AuthContext } from '../../context/auth'
 
 const CapitaesCampeonato = () => {
 
     const { id } = useParams()
+    const [loading, setLoading] = useState(true)
     const [campeonato, setCampeonato] = useState({})
     const [capitaes, setCapitaes] = useState([])
 
     useEffect(() => {
         const getCapitaes = async () => {
             try {
-                const {data: capitaes} = await Api.get(`/campeonatos/time/times/capitaes/${id}`)
-                const {data: capitaesOitavas} = await Api.get(`/campeonatos/time/times/capitaes/${id}/fase/oitavas`)
-                
-                if(capitaes.length > 16){
+                const { data: capitaes } = await Api.get(`/campeonatos/time/times/capitaes/${id}`)
+                const { data: capitaesOitavas } = await Api.get(`/campeonatos/time/times/capitaes/${id}/fase/oitavas`)
+
+                if (capitaes.length > 16) {
                     setCapitaes(capitaesOitavas)
-                }else{
+                } else {
                     setCapitaes(capitaes)
                 }
+
+                setLoading(false)
             } catch (e) {
                 console.error(e)
             }
@@ -42,40 +47,53 @@ const CapitaesCampeonato = () => {
         <>
             <h1>{campeonato.nome}</h1>
             <h2 className="h2-sublinhado">CAPITÃES DOS TIMES</h2>
-
             {
-                capitaes.length > 0 
-                
-                ?
+                loading ?
+                    (
+                        <Center>
+                            <Spinner
+                                thickness='4px'
+                                speed='0.65s'
+                                color='#fff'
+                                size='xl'
+                            />
+                        </Center>
+                    )
+                    :
+                    (
+                        capitaes.length > 0
 
-            <TableContainer>
-                <Table size='md'>
-                    <Thead >
-                        <Tr>
-                            <Th color={'#7662F1'}>Nome</Th>
-                            <Th color={'#7662F1'}>Email</Th>
-                            <Th color={'#7662F1'}>Time</Th>
-                        </Tr>
-                    </Thead>
-                    
-                    <Tbody>
-                        {
-                            capitaes.map((capitain) => (
-                                    <Tr key={capitain.id_time}>
-                                        <Td>{capitain.nome_usuario}</Td>
-                                        <Td>{capitain.celular ? capitain.celular : "Sem celular cadastrado"}</Td>
-                                        <Td>{capitain.nome_time}</Td>
-                                    </Tr>
-                            ))
+                            ?
 
-                        }
-                    </Tbody>
-                </Table>
-            </TableContainer>
+                            <TableContainer>
+                                <Table size='md'>
+                                    <Thead >
+                                        <Tr>
+                                            <Th color={'#7662F1'}>Nome</Th>
+                                            <Th color={'#7662F1'}>Email</Th>
+                                            <Th color={'#7662F1'}>Time</Th>
+                                        </Tr>
+                                    </Thead>
 
-            :
+                                    <Tbody>
+                                        {
+                                            capitaes.map((capitain) => (
+                                                <Tr key={capitain.id_time}>
+                                                    <Td>{capitain.nome_usuario}</Td>
+                                                    <Td>{capitain.celular ? capitain.celular : "Sem celular cadastrado"}</Td>
+                                                    <Td>{capitain.nome_time}</Td>
+                                                </Tr>
+                                            ))
 
-            <h2>Sem times ainda..</h2>
+                                        }
+                                    </Tbody>
+                                </Table>
+                            </TableContainer>
+
+                            :
+
+                            <h2>Nenhum time foi cadastrado nesse campeonato!</h2>
+                    )
 
             }
 
