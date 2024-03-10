57f27688a51fdcfb114a8409f7a210790a44a087
zaiaaa <gustavo.0801214@gmail.com>
2024-03-09 22:20:44 -0300

    fazendo a pagina de valores por campeonato

--- src/pages/valoresCampeonato/ValoresCampeonato.jsx
+++ src/pages/valoresCampeonato/ValoresCampeonato.jsx
@@ -2,19 +2,26 @@ import { useParams } from "react-router-dom"
 import { Api } from "../../services/api"
 import { useEffect, useState } from "react"
 import { CardValores } from "../../components/CardValores/CardValores"
+import { formataDinheiro } from "../editarCampeonato/EditarCampeonato"
 
 const ValoresCampeonato = () => {
 
     const { id } = useParams()
     const [campeonato, setCampeonato] = useState({})
+    const [valoresCamp, setValoresCamp] = useState(0)
+    const [jogadoresInscritos, setJogadoresInscritos] = useState(0)
 
     useEffect(() => {
 
         const getCamepeonatoData = async () => {
             try {
                 const fetch = await Api.get(`campeonatos/id/${id}`)
-                console.log(fetch)
                 setCampeonato(fetch.data[0])
+                const {data: valores} = await Api.get(`/campeonatos/pagamentos/${id}`)
+                setValoresCamp(valores[0].valor_total)
+
+                const {data: inscritos} = await Api.get(`/campeonatos/time/times/${id}`)
+                setJogadoresInscritos(inscritos.length)
             } catch (e) {
                 console.log(e)
             }
@@ -28,10 +35,11 @@ const ValoresCampeonato = () => {
             <h1>{campeonato.nome}</h1>
             <CardValores titulo={'VALORES'}>
                 <ul>
-                    <li>VALOR ARRECADADO: <span className="valores">111.10  R$</span></li>
-                    <li>VALOR EM PREMIAÇÕES: <span className="valores">1111</span></li>
-                    <li>VALOR DA INSCRIÇÃO: <span className="valores">1111</span></li>
-                    <li>TIMES INSCRITOS: <span className="times-inscritos">1111</span></li>
+                    
+                    <li>VALOR ARRECADADO: <span className="valores">{formataDinheiro(valoresCamp)}</span></li>
+                    <li>VALOR EM PREMIAÇÕES: <span className="valores">{formataDinheiro(campeonato.premiacao)}</span></li>
+                    <li>VALOR DA INSCRIÇÃO: <span className="valores">{formataDinheiro(campeonato.valor_entrada)}</span></li>
+                    <li>TIMES INSCRITOS: <span className="times-inscritos">{jogadoresInscritos}</span></li>
                 </ul>
             </CardValores>
         </>
