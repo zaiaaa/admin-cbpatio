e8820065efdce932e932f201dc21b17874cd91e9
zaiaaa <gustavo.0801214@gmail.com>
2024-03-20 17:33:09 -0300

    colocando loader no setar live

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,10 +5,12 @@ import { useContext, useEffect, useState } from 'react';
 import { Button } from '../Button/button';
 import { AuthContext } from '../../context/auth';
 import { Api } from '../../services/api';
+import { Spinner } from '@chakra-ui/react';
 
 const Layout = ({ children }) => {
 
     const {logoff} = useContext(AuthContext)
+    const [loading, setLoading] = useState(false)
 
     const menuItem = document.querySelectorAll('.link');
     function selectLink() {
@@ -25,6 +27,7 @@ const Layout = ({ children }) => {
     const [isliveOn, setIsLiveOn] = useState(false)
 
     const handleLiveOn = async () => {
+        setLoading(true)
         if(isliveOn == false){
             await Api.put('/liveon/1', {
                 live_on: "s"
@@ -36,6 +39,7 @@ const Layout = ({ children }) => {
         }
         window.location.reload()
         alert("live setada com sucesso!")
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -69,7 +73,11 @@ const Layout = ({ children }) => {
 
                         </ul>
                     </nav>
-                    <Button text={ !isliveOn ? 'live on?' : 'live off?'} type={"button"} variant={"purple"} width={"100%"} padding={".75rem 2rem"} onClick={handleLiveOn} />
+                    <Button text={ loading ? <Spinner 
+                            thickness='4px'
+                            speed='0.65s'
+                            color='#fff'
+                            size='xl'/> : !isliveOn ? 'live on?' : 'live off?'} type={"button"} variant={"purple"} width={"100%"} padding={".75rem 2rem"} onClick={handleLiveOn} />
                     <Button text={'Sair'} type={"button"} variant={"red"} width={"100%"} padding={".75rem 2rem"} onClick={logoff} />
                 </div>
                 <div className="children">
